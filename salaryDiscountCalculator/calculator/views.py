0bfea7b0bd2a194e4e73b1ec95c788dfa4cb62bd
cturbides <c.sturbides@gmail.com>
2022-10-23 12:08:24 -0400

    feat: SECRET_KEY into .env file and static moved to root dir

--- salaryDiscountCalculator/calculator/views.py
+++ salaryDiscountCalculator/calculator/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-def main():
-    pass
+def main(request):
+    if request.method == 'POST':
+        return
+    return render(request, 'calculator/index.html')
